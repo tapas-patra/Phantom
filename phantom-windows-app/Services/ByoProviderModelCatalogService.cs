527e9e3fad0ecc4fcaef3b23bee7a19520148913
Tapas Kumar Patra <tapas.patra0406@gmail.com>
2026-06-18 01:28:24 +0530

    fix 2.1.2

--- phantom-windows-app/Services/ByoProviderModelCatalogService.cs
+++ phantom-windows-app/Services/ByoProviderModelCatalogService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using SecureOverlay.Helpers;
 using SecureOverlay.Infrastructure.Hosted.Contracts;
 
 namespace SecureOverlay.Services
